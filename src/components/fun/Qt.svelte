4b228a809b64674093136e67096a2671017d3d38
dragsbruh <dragsbruh@protonmail.com>
2025-11-01 22:23:40 +0530

    minor changes i again cant be bothered to write down

--- src/components/fun/Qt.svelte
+++ src/components/fun/Qt.svelte
@@ -6,10 +6,12 @@
   onMount(() => (qt = qts[Math.floor(Math.random() * qts.length)]));
 </script>
 
-<div class="fixed right-0 bottom-0">
-  <img
-    alt="furina"
-    src="https://catbox.moe/pictures/qts/{qt}"
-    class="max-w-48"
-  />
-</div>
+{#if qt}
+  <div class="fixed right-0 bottom-0">
+    <img
+      alt="furina"
+      src="https://catbox.moe/pictures/qts/{qt}"
+      class="max-w-48"
+    />
+  </div>
+{/if}
